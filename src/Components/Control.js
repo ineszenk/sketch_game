8ba6331db560699d576a17db26069d754b493bc9
Ines <inz@e-nno.ch>
2020-11-06 20:22:05 +0100

    [FIX] prediction

--- src/Components/Control.js
+++ src/Components/Control.js
@@ -9,24 +9,19 @@ function Controls({ theCanvas, model, labels }) {
   let [predict, setPredict] = useState(false);
   const [timer, seconds] = useTimer({ predict, setPredict });
 
-  let [Prediction, setPrediction] = useState(""); // Sets default label to empty string.
-
   let { next, current, dispatch } = useContext(GameContext);
 
   useEffect(() => {
     if (current + 1 < 10) {
       clearInterval(timer);
+    }
+
+    if (seconds === 0) {
       getPrediction(theCanvas, model).then(prediction => {
-        setPrediction(labels[prediction[0]]);
-        if (labels[prediction[0]] === labels[current]) {
-          seconds <= 5
-            ? dispatch({ type: "increment" })
-            : dispatch({ type: "bonus" });
-          next();
-          setPredict(true);
-        } else {
-          console.log("Try Again !");
-        }
+        console.log(prediction[0]);
+        labels[prediction[0]] === labels[current]
+          ? dispatch({ type: "increment" })
+          : console.log("Try Again !");
       });
     }
   });
@@ -40,13 +35,13 @@ function Controls({ theCanvas, model, labels }) {
             onClick={() => {
               if (current + 1 <= 10) {
                 getPrediction(theCanvas, model).then(prediction => {
-                  setPrediction(labels[prediction[0]]);
                   setPredict(true);
                   if (labels[prediction[0]] === labels[current]) {
                     seconds <= 5
                       ? dispatch({ type: "increment" })
                       : dispatch({ type: "bonus" });
                     next();
+                    setPredict(true);
                   } else {
                     alert("Sorry but no...try harder next round !");
                     next();
--- src/timer.js
+++ src/timer.js
@@ -2,16 +2,16 @@ import { useContext, useState } from "react";
 import { GameContext } from "./Game";
 
 export function useTimer({ setPredict, predict }) {
-  const [seconds, setSeconds] = useState(20);
+  const [seconds, setSeconds] = useState(10);
   let { next } = useContext(GameContext);
 
   const timer = setTimeout(() => {
     if (seconds === 0) {
-      setSeconds(20);
+      setSeconds(10);
       next();
     }
     if (predict && seconds !== 0) {
-      setSeconds(20);
+      setSeconds(10);
       setPredict(false);
     } else if (!predict && seconds !== 0) {
       setSeconds(seconds - 1);
